f70b2378f1e319bac0341a1c065c3a53528abe00
malene <maleneva@hotmail.com>
2022-02-24 16:03:54 +0100

    updated code

--- resnet50Config.py
+++ resnet50Config.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+# import os
+# import cv2
+import argparse
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model, Sequential
@@ -14,49 +16,50 @@ from tensorflow.keras import optimizers
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-t","--training", required=True, help="Path to training directory")
-ap.add_argument("-v","--validation", required=True, help="Path to validation directory")
+ap.add_argument("-e", "--epochs", type =int, default = 20, help ="Number of epochs for training")
+ap.add_argument("-b", "--batchsize", type=int, default =25, help = "Number of batch size")
+# ap.add_argument("-v","--validation", required=True, help="Path to validation directory")
 args = vars(ap.parse_args())
 
 #Defined path for training new weights for the top layer
-resnet50_weights_path = 'C:\\Users\\Malene\\OneDrive - NTNU\\Documents\\NTNU\\MasterThesis-2022\\Code-testing\\master_test\\resnet50_weights_tf_kernels_notop.h5'
-training_path= 'C:\\Users\\Malene\\OneDrive - NTNU\\Documents\\NTNU\\MasterThesis-2022\\Code-testing\\master_test\\image_path\\train'
-validation_path= 'C:\\Users\\Malene\\OneDrive - NTNU\\Documents\\NTNU\\MasterThesis-2022\\Code-testing\\master_test\\image_path\\validation'
+resnet50_pretrained_weights_notop = 'C:\\Users\\Malene\\.keras\\models\\resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+resnet50_pretrained_weights = 'C:\\Users\\Malene\\.keras\\models\\resnet50_weights_tf_dim_ordering_tf_kernels.h5'
 
-EPOCHS = 20
-BATCH_SIZE = 5
+# training_path= 'C:\\Users\\Malene\\OneDrive - NTNU\\Documents\\NTNU\\MasterThesis-2022\\Code-testing\\master_test\\image_path\\train'
+# validation_path= 'C:\\Users\\Malene\\OneDrive - NTNU\\Documents\\NTNU\\MasterThesis-2022\\Code-testing\\master_test\\image_path\\validation'
+
+EPOCHS = args["epochs"]
+BATCH_SIZE = args["batchsize"]
 TESTING_SIZE = 0.9
-VALIDATION_SIZE = 0.1
+VALIDATION_SIZE = 0.15
 IMG_SIZE = 224
+SEED_VALUE = 30
 
 
 #Decalre dataset for training split
-# img_generator = keras.preprocessing.image.ImageDataGenerator(
-train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+train_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input,
+    validation_split=VALIDATION_SIZE)
 
 #Don't know if I need further preprocessing here:
-# train_img_generator = img_generator.flow_from_directory(
 train_img_generator = train_datagen.flow_from_directory(
-    directory = training_path,
+    directory = args["training"],
     target_size=(224,224),
     batch_size = BATCH_SIZE,
-    # class_mode = 'categorical',
     class_mode = 'binary',
+    seed = SEED_VALUE,
     subset = 'training'
-    # shuffle = True,
     )
-# print(train_img_generator[0])
 
-# img_validation_generator = keras.preprocessing.image.ImageDataGenerator(
-img_validation_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+img_validation_generator = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=VALIDATION_SIZE)
 #Declare dataset for validation split
 validation_img_generator = img_validation_generator.flow_from_directory(
-    directory = validation_path,
+    directory = args["training"],
     target_size = (224,224),
-    batch_size = BATCH_SIZE,
-    # class_mode = 'categorical',
+    batch_size = 10,
     class_mode = 'binary',
+    seed = SEED_VALUE,
     subset = 'validation'
-    # shuffle = True,
     )
 
 #Future TODO when tweaking model:
@@ -67,58 +70,58 @@ callback_args = [
 
 #---------------------------------- MODEL --------------------------------
 
-
-# model.add(tf.keras.layers.ZeroPadding2D((0,0), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
-
-# model.add(ResNet50(
-# model = ResNet50(
-#     # include_top=True,
-#     include_top=False,  #removes the last dense layer in the pretrained model
-#     weights="imagenet",
-#     # input_tensor=inputTensor,
-#     input_tensor=None,
-#     input_shape=(224, 224, 3),
-#     pooling='avg',
-#     # classes=1,
-# )
-# ))
-# output = model.layers[0].output
-# output = keras.layers.Flatten()(output)
-# model = Model(model.input, output = output)
-#
-# for layer in model.layers:
-#     layer.trainable = False
-
-# model.summary()
-
 #Add resnet50 model to the sequential model
 # inputTensor = keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
 model = Sequential()
-model.add(ResNet50(include_top=False, pooling='avg', weights='imagenet'))
+model.add(ResNet50(
+    include_top=False,
+    pooling='avg',
+    weights='imagenet',
+    # weights = resnet50_pretrained_weights_notop
+    )
+)
 #Adding a Dense layer with the two classes forged and valid images
-model.add(Dense(2, activation='softmax'))
-# model.add(Dense(1, activation='sigmoid'))
+# model.add(Dense(2, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))   #Sigmoid is used for binary classification
 model.layers[0].trainable=False
 #The first layer is not trained with new weights from this project
-# model.layers[0].trainable = False
 model.summary()
 
 #Define optimizer function
 adam = tf.keras.optimizers.Adam(lr = 0.01, decay = 1e-6)
 model.compile(
     optimizer = adam,
-    # loss="binary_crossentropy",
-    loss="sparse_categorical_crossentropy",
+    loss="binary_crossentropy",
+    # loss="sparse_categorical_crossentropy",
     metrics=['accuracy'])
 
-model_history = model.fit(
+history = model.fit(
     train_img_generator,
     epochs=EPOCHS,
     batch_size=BATCH_SIZE,
     verbose = 1,
     validation_data=validation_img_generator
-    #callbacks?
-    # loss
 )
 
 model.summary()
+
+
+
+
+
+#Plot model accuracy in graph
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(acc))
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+plt.legend(['Train','Val'])
+plt.figure() #Train accuracy
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+plt.legend(['Train','Val']) #Train Loss
+plt.show()
