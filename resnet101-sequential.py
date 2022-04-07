678d716442e36909ee8fd80ec6fb7f32f7927836
malene <maleneva@hotmail.com>
2022-04-07 11:57:02 +0200

    changed variables in resnet101 model

--- resnet101-sequential.py
+++ resnet101-sequential.py
@@ -114,9 +114,9 @@ output = keras.layers.GlobalAveragePooling2D()(output)
 output = keras.layers.Flatten()(output)
 # TODO: test different regularizers
 
-output = keras.layers.Dense(512, activation='relu',  kernel_regularizer=regularizers.l2(0.004))(output)
+output = keras.layers.Dense(512, activation='relu',  kernel_regularizer=regularizers.l2(0.003))(output)
 output = keras.layers.Dropout(0.25)(output)
-# output = keras.layers.BatchNormalization()(output)
+output = keras.layers.BatchNormalization()(output)
 
 # TODO: test out different Dropout
 # output = keras.layers.Dense(512)(output)
